50091ed9cd416aef3ccdc47dee4971001768841e
root <andrea.munozgr@gmail.com>
2025-08-06 05:58:56 +0000

    Adding main and script 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -24,5 +24,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (cont);
+
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
 }
