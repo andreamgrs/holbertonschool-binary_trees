40c2038fcb7e4369a0f18e36e09eab8525312b29
root <andrea.munozgr@gmail.com>
2025-08-06 06:29:06 +0000

    Adding main and script 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,32 +10,15 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth, right_depth;
+	size_t depth;
 
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 	{
 		return (0);
 	}
-
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (max_depth);
-	}
-
-	if (tree->parent == NULL)
-	{
-		return (max_depth - max_depth);
-	}
-
-	left_depth = binary_tree_depth(tree->left);
-	right_depth = binary_tree_depth(tree->right);
-
-	if (left_depth > right_depth)
-	{
-		max_depth = left_depth + 1;
-	}
 	else
 	{
-		max_depth = right_depth + 1;
+		depth = binary_tree_depth(tree->parent) + 1;
+		return (depth);
 	}
 }
