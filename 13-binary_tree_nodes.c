4073e7c7385d93166f8c9c281819723523e7ba9c
root <andrea.munozgr@gmail.com>
2025-08-07 00:48:40 +0000

    Adding main and script 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,21 +12,27 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t leaves_left, leaves_right;
+	size_t nodes_left, nodes_right;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	if (tree->left == NULL && tree->right == NULL)
+	nodes_left = binary_tree_nodes(tree->left);
+	nodes_right = binary_tree_nodes(tree->right);
+
+	if (tree->parent == NULL)
 	{
-		return (1);
+		return (nodes_left + nodes_right + 1);
+	}
+
+	if ((tree->left != NULL && tree->right == NULL)||(tree->left == NULL && tree->right != NULL))
+	{
+		return (1 + nodes_left + nodes_right);
 	}
 	else
 	{
-		leaves_left = binary_tree_leaves(tree->left);
-		leaves_right = binary_tree_leaves(tree->right);
-		return (leaves_left + leaves_right);
+		return (nodes_left + nodes_right);
 	}
-}
-~      
+	
+}      
